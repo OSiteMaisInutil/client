3aff148d47fa1cc1001c26d050c76225a9b5f7d0
Hoyasumii <alanreisanjo@gmail.com>
2024-08-23 17:40:16 -0300

    Ajustes

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
           Seja bem-vind@ ao site mais inútil. Eu não sei o que você está fazendo
           aqui, mas sei que você não tem o que fazer(venhamos e convenhamos).
           Aqui estão algumas ferramentas de futilidade para você se divertir(ou
-          tentar).a
+          tentar).
         </p>
         <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-4 md:gap-8">
           <CategoryCard src="/public/dev.png" className="col-span-1">
