9f31addde39538311d32069c0d448c833e31c5f8
Hoyasumii <alanreisanjo@gmail.com>
2024-08-23 17:39:41 -0300

    Ajustes

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import module from "./index.module.scss";
 export function Header() {
   return (
     <div className={module.header}>
-      <strong>o site mais Inútil</strong>
+      <strong>o site mais inútil</strong>
     </div>
   );
 }
